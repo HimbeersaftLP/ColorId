77b2de68af4744cc138f63b929acd1f798907415
HimbeersaftLP <elias.nestl@outlook.de>
2016-06-27 19:32:34 +0200

    Update main.php

--- src/HimbeersaftLP/Wooler/main.php
+++ src/HimbeersaftLP/Wooler/main.php
@@ -56,46 +56,64 @@ class Main extends PluginBase implements Listener{
      public function onCommand(CommandSender $sender, Command $command, $label, array $args){
           switch($command->getName()){
                case "wool":
-                    if(!isset(args[0])){
+                    if(!isset($args[0])){
                          return false;
+                         break;
                     }else{
                          switch($args[0]){
                               case "id":
                                    if(!isset($args[1])){
                                         return false;
+                                        break;
                                    }else{
                                         $meta = $this->getWoolMeta(strtolower($args[1]));
                                         if($meta === false){
                                              $sender->sendMessage(TF::RED . "Color " . $args[1] . " not found!");
+                                             break;
                                         }else{
                                              $sender->sendMessage("The ID of " . $args[1] . " wool is " . TF::AQUA . "35" . TF::WHITE . " and the damage tag is ". TF::AQUA . $meta . TF::WHITE . "\nShort form: 35:" . $meta);
+                                             break;
                                         }
+                                        break;
                                    }
+                                   break;
                               case "give":
                                    if($sender instanceof Player){
                                         if(!isset($args[1])){
                                              return false;
+                                             break;
                                         }else{
                                              $meta = $this->getWoolMeta(strtolower($args[1]));
                                              if($meta === false){
                                                   $sender->sendMessage(TF::RED . "Color " . $args[1] . " not found!");
+                                                  break;
                                              }else{
-                                                  if(isset($args[2])){
+                                                  if(isset($args[2]) && is_numeric($args[2])){
                                                        $count = $args[2];
                                                   }else{
                                                        $count = 1;
                                                   }
                                                   $sender->getInventory()->addItem(Item::get(35,$meta,$count));
                                                   $sender->sendMessage("Wool with ID 35:" . $meta . " has been successfully added to your inventory");
+                                                  break;
                                              }
+                                             break;
                                         }
+                                        break;
                                    }else{
-                                        $sender->sendMessage(TF::RED . "Please use this command ingame!")
+                                        $sender->sendMessage(TF::RED . "Please use this command ingame!");
+                                        break;
                                    }
+                                   break;
                               case "list":
                                    $sender->sendMessage("List of all wool colors with IDs:\nWhite: 35:0\n".TF::GOLD."Orange: 35:1\n".TF::LIGHT_PURPLE."Magenta: 35:2\n".TF::AQUA."Light blue: 35:3\n".TF::YELLOW."Yellow: 35:4\n".TF::GREEN."Lime: 35:5\n".TF::LIGHT_PURPLE."Pink: 35:6\n".TF::DARK_GRAY."Gray: 35:7\n".TF::GRAY."Light gray: 35:8\n".TF::DARK_AQUA."Cyan: 35:9\n".TF::DARK_PURPLE."Purple: 35:10\n".TF::BLUE."Blue: 35:11\n".TF::GOLD."Brown: 35:12\n".TF::DARK_GREEN."Green: 35:13\n".TF::RED."Red: 35:14\n".TF::BLACK."Black: 35:15");
+                                   break;
+                              default:
+                                   return false;
+                                   break;
                          }
                     }
+                    break;
           }
           return true;
      }
